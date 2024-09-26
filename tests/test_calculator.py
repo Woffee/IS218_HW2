c1ca353eabc330558133d2594af0a4578139dabe
woffee <iwoffee@gmail.com>
2024-09-26 10:42:58 -0400

    update tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,9 @@
 '''My Calculator Test'''
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from calculator import add, subtract
 
 def test_addition():
@@ -8,3 +13,9 @@ def test_addition():
 def test_subtraction():
     '''Test that addition function works '''    
     assert subtract(2,2) == 0
+
+
+if __name__ == '__main__':
+    test_addition()
+    test_subtraction()
+    print('All tests passed')
